36e4fd41d998d8506abc21e02cd4b41d5b5aae2d
nickgoodmanbmstu <56290374+nickgoodmanbmstu@users.noreply.github.com>
2019-11-05 09:56:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+<?php
+
+$rows = 10; // количество строк, tr
+$cols = 10; // количество столбцов, td
+
+echo '<table border="1">';
+
+for ($tr=1; $tr<=$rows; $tr++){ 
+    echo '<tr>';
+    for ($td=1; $td<=$cols; $td++){
+        echo '<td>'. $tr*$td .'</td>';
+    }
+    echo '</tr>';
+}
+
+echo '</table>';
+
+?>
+
+
+
 
